632f0ec02ada389606829ce59fa5fe8cb2f3b6b4
oivalian <109859213+oivalian@users.noreply.github.com>
2024-02-02 18:34:38 +1100

    Updated upgrades to be relative
    +1 = 100
    +10 = 1000
    + 100 = 10000
    + 1K = 100000

--- clicker.py
+++ clicker.py
@@ -7,12 +7,12 @@ class Events:
         self.count = 0
         self.multiplier = 1
         self.x1_count = 0
-        self.x4_count = 0
-        self.x8_count = 0
         self.x10_count = 0
+        self.x100_count = 0
+        self.x1000_count = 0
 
     def counters(self):
-        upgrade_buttons = [upgrade_x2, upgrade_x4, upgrade_x8, upgrade_x10]
+        upgrade_buttons = [upgrade_x1, upgrade_x10, upgrade_x100, upgrade_x1000]
         save_label.forget()
         load_label.forget()
         for i, button in enumerate(upgrade_buttons):
@@ -33,29 +33,29 @@ class Events:
             clicker_count_var.set(self.count)
             self.totals()
 
-    def upgrade_x4(self):
-        self.x4_count += 1
+    def upgrade_x10(self):
+        self.x10_count += 1
         if self.count >= 1000:
             self.count -= 1000
-            self.multiplier += 4
+            self.multiplier += 10
             upgrade_x4.config(state="disabled") if self.count < 1000 else None
             clicker_count_var.set(self.count)
             self.totals()
 
-    def upgrade_x8(self):
-        self.x8_count += 1
+    def upgrade_x100(self):
+        self.x100_count += 1
         if self.count >= 10000:
             self.count -= 10000
-            self.multiplier += 8
+            self.multiplier += 100
             upgrade_x8.config(state="disabled") if self.count < 10000 else None
             clicker_count_var.set(self.count)
             self.totals()
 
-    def upgrade_x10(self):
-        self.x10_count += 1
+    def upgrade_x1000(self):
+        self.x1000_count += 1
         if self.count >= 100000:
             self.count -= 100000
-            self.multiplier += 10
+            self.multiplier += 100
             upgrade_x10.config(state="disabled") if self.count < 100000 else None
             clicker_count_var.set(self.count)
             self.totals()
@@ -63,8 +63,8 @@ class Events:
     def save_file(self):
         try:
             with open("save_data.dat", "wb") as file:
-                pickle.dump([self.count, self.multiplier, self.x1_count, self.x4_count,
-                             self.x8_count, self.x10_count], file)
+                pickle.dump([self.count, self.multiplier, self.x1_count, self.x10_count,
+                             self.x100_count, self.x1000_count], file)
                 save_label.pack(side="bottom")
                 self.totals()
         except FileNotFoundError as error_code:
@@ -73,13 +73,13 @@ class Events:
     def load_file(self):
         try:
             with open("save_data.dat", "rb") as file:
-                count, multiplier, x1, x4, x8, x10 = pickle.load(file)
+                count, multiplier, x1, x10, x100, x1000 = pickle.load(file)
                 self.count = count
                 self.multiplier = multiplier
                 self.x1_count = x1
-                self.x4_count = x4
-                self.x8_count = x8
                 self.x10_count = x10
+                self.x100_count = x100
+                self.x1000_count = x1000
                 clicker_count_var.set(self.count)
                 load_label.pack(side="bottom")
                 self.totals()
@@ -89,9 +89,9 @@ class Events:
     def totals(self):
         totals = f"Total Clicks = {self.multiplier} \
                     Total +1 = {self.x1_count} \
-                    Total +4 = {self.x4_count} \
-                    Total +8 = {self.x8_count} \
-                    Total +10 = {self.x10_count}"
+                    Total +10 = {self.x10_count} \
+                    Total +100 = {self.x100_count} \
+                    Total +1K = {self.x1000_count}"
         totals_var.get()
         totals_var.set(totals)
 
@@ -119,14 +119,14 @@ if __name__ == "__main__":
 
     # upgrade area
     upgrade_frame = ttk.Frame(root)
-    upgrade_x2 = ttk.Button(upgrade_frame, text="+1 (c: 100)\n(Hotkey: F1)", command=clicker_instance.upgrade_x1)
+    upgrade_x1 = ttk.Button(upgrade_frame, text="+1 (c: 100)\n(Hotkey: F1)", command=clicker_instance.upgrade_x1)
     clicker.bind("<F1>", lambda event: clicker_instance.upgrade_x1())
-    upgrade_x4 = ttk.Button(upgrade_frame, text="+4 (c: 1K)\n(Hotkey: F2)", command=clicker_instance.upgrade_x4)
-    clicker.bind("<F2>", lambda event: clicker_instance.upgrade_x4())
-    upgrade_x8 = ttk.Button(upgrade_frame, text="+8 (c: 10K)\n(Hotkey: F3)", command=clicker_instance.upgrade_x8)
-    clicker.bind("<F3>", lambda event: clicker_instance.upgrade_x8())
-    upgrade_x10 = ttk.Button(upgrade_frame, text="+10 (c: 100K)\n(Hotkey: F4)", command=clicker_instance.upgrade_x10)
-    clicker.bind("<F4>", lambda event: clicker_instance.upgrade_x10())
+    upgrade_x10 = ttk.Button(upgrade_frame, text="+10 (c: 1K)\n(Hotkey: F2)", command=clicker_instance.upgrade_x10)
+    clicker.bind("<F2>", lambda event: clicker_instance.upgrade_x10())
+    upgrade_x100 = ttk.Button(upgrade_frame, text="+100 (c: 10K)\n(Hotkey: F3)", command=clicker_instance.upgrade_x100)
+    clicker.bind("<F3>", lambda event: clicker_instance.upgrade_x100())
+    upgrade_x1000 = ttk.Button(upgrade_frame, text="+1K (c: 100K)\n(Hotkey: F4)", command=clicker_instance.upgrade_x1000)
+    clicker.bind("<F4>", lambda event: clicker_instance.upgrade_x1000())
 
     # totals area
     totals_frame = ttk.Frame(root)
@@ -146,10 +146,10 @@ if __name__ == "__main__":
 
     # Upgrades Package
     upgrade_frame.grid(row=1, column=0, pady=50, padx=10, sticky="nsew")
-    upgrade_x2.pack(ipady=10, ipadx=60, pady=5)
-    upgrade_x4.pack(ipady=10, ipadx=60, pady=5)
-    upgrade_x8.pack(ipady=10, ipadx=60, pady=5)
-    upgrade_x10.pack(ipady=10, ipadx=55, pady=5)
+    upgrade_x1.pack(ipady=10, ipadx=60, pady=5)
+    upgrade_x10.pack(ipady=10, ipadx=60, pady=5)
+    upgrade_x100.pack(ipady=10, ipadx=55, pady=5)
+    upgrade_x1000.pack(ipady=10, ipadx=55, pady=5)
 
     # Clicker Package
     clicker_frame.grid(row=1, column=1, pady=50, padx=10, sticky="nsew")
@@ -164,10 +164,10 @@ if __name__ == "__main__":
     footer_frame.grid(row=3, column=0, columnspan=2, pady=20, padx=10, sticky="nsew")
 
     # loop
-    upgrade_x2.config(state="disabled")
-    upgrade_x4.config(state="disabled")
-    upgrade_x8.config(state="disabled")
+    upgrade_x1.config(state="disabled")
     upgrade_x10.config(state="disabled")
+    upgrade_x100.config(state="disabled")
+    upgrade_x1000.config(state="disabled")
     
     clicker_instance.totals()
     clicker_instance.load_file()
